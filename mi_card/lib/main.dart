2faf1ca90fae4b4eff919b1de0d03ec59e4c41b7
cong dinh <congdinhpro2k@gmail.com>
2023-06-25 16:29:02 +0700

    complete mi_card

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -13,18 +13,115 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage("images/cat.jpg"),
               ),
-              Text(
+              const Text(
                 'Meow Meow',
                 style: TextStyle(
                     color: Colors.white,
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
                     fontFamily: "Pacifico"),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Code Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              // Container(
+              //   color: Colors.white,
+              //   padding: EdgeInsets.all(10.0),
+              //   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              //   child: Row(
+              //     children: [
+              //       const Icon(
+              //         Icons.phone,
+              //         color: Colors.teal,
+              //       ),
+              //       const SizedBox(
+              //         width: 10,
+              //       ),
+              //       Text(
+              //         '+84 965 027 356',
+              //         style: TextStyle(
+              //             color: Colors.teal.shade900,
+              //             fontFamily: 'Source Code Pro',
+              //             fontSize: 20.0),
+              //       )
+              //     ],
+              //   ),
+              // ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+84 965 027 356',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 20.0),
+                    )),
+              ),
+              // Container(
+              //   color: Colors.white,
+              //   padding: const EdgeInsets.all(10.0),
+              //   margin: const EdgeInsets.symmetric(
+              //       vertical: 10.0, horizontal: 25.0),
+              //   child: Row(
+              //     children: [
+              //       const Icon(
+              //         Icons.email,
+              //         color: Colors.teal,
+              //       ),
+              //       const SizedBox(
+              //         width: 10,
+              //       ),
+              //       Text(
+              //         'congdinh.work@gmail.com',
+              //         style: TextStyle(
+              //             color: Colors.teal.shade900,
+              //             fontFamily: 'Source Code Pro',
+              //             fontSize: 20.0),
+              //       )
+              //     ],
+              //   ),
+              // ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'congdinh@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Code Pro',
+                        fontSize: 20.0),
+                  ),
+                ),
               )
             ],
           ),
