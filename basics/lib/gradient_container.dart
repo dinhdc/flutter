ae4f107bc125bad4208e5b52eaf54afeaa152173
dinh.dc <dinh.dc@idtinc.co>
2024-01-08 13:55:22 +0700

    add random function and convert to statefull widget

--- basics/lib/gradient_container.dart
+++ basics/lib/gradient_container.dart
@@ -1,37 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   const GradientContainer({required this.colors, super.key});
 
   final List<Color> colors;
+
+  @override
+  State<GradientContainer> createState() => _GradientContainerState();
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+  int diceNumber = 1;
+
+  int rollDice(){
+    return Random().nextInt(6) + 1;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
-                colors: colors, begin: startAlignment, end: endAlignment)),
+                colors: widget.colors, begin: startAlignment, end: endAlignment)),
         child: Center(
             child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
-              'assets/images/dice-1.png',
+              'assets/images/dice-$diceNumber.png',
               width: 200.0,
             ),
             const SizedBox(
               height: 20,
             ),
             TextButton(
-              onPressed: () {},
-              child: const Text('Roll Dice'),
+              onPressed: () {
+                setState(() {
+                  diceNumber = rollDice();
+                });
+              },
               style: TextButton.styleFrom(
                   // padding: EdgeInsets.only(top: 20),
                   foregroundColor: Colors.white,
                   textStyle: const TextStyle(fontSize: 28)),
+              child: const Text('Roll Dice'),
             )
           ],
         )));
