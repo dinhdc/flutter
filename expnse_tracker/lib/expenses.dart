7e83af9bca5fed9c7779cbd7439ffbad7c2e0d63
dinh.dc <dinh.dc@idtinc.co>
2024-01-09 13:33:18 +0700

    update expense_list component

--- expnse_tracker/lib/expenses.dart
+++ expnse_tracker/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expnse_tracker/components/expense_list.dart';
+import 'package:expnse_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -8,11 +10,24 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+        title: 'Flutter course',
+        amount: 199000,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 120000,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      children: [Text('The chart'), Text('Expense ')],
+      children: [Text('The chart'), Expanded(child: ExpenseList(expenses: _registeredExpense))],
     ));
   }
 }
--- expnse_tracker/lib/models/expense.dart
+++ expnse_tracker/lib/models/expense.dart
@@ -1,9 +1,20 @@
-class Expense {
+import 'package:uuid/uuid.dart';
 
-  const Expense({required this.title, required this.amount});
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
