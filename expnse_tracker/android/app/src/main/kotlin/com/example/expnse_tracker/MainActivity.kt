c2321652d114a46f3fc130f85ea5da357bb6976c
dinh.dc <dinh.dc@idtinc.co>
2024-01-09 11:41:06 +0700

    init expense tracker app

---
+++ expnse_tracker/android/app/src/main/kotlin/com/example/expnse_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expnse_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ expnse_tracker/lib/models/expense.dart
@@ -0,0 +1,9 @@
+class Expense {
+
+  const Expense({required this.title, required this.amount});
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
